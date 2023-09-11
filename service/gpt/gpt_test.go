a240a8d1efe7bc20b408de870ad1bf3b8a48f9be
heimeropen <heimer@oppenheimer.top>
2023-09-11 22:45:28 +0800

    fix unit test

--- service/gpt/gpt_test.go
+++ service/gpt/gpt_test.go
@@ -25,7 +25,7 @@ func TestMain(m *testing.M) {
 }
 
 func TestListModels(t *testing.T) {
-	models, err := gpt.Client().ListModels(context.Background())
+	models, err := gpt.GetClient().GoGptClient.ListModels(context.Background())
 	fmt.Println(err)
 	fmt.Println(models)
 
